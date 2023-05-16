46c078b8f81f2dd31b70dba4513482641e53a569
Adreyan Distor <adreyandistor@gmail.com>
2023-05-16 13:08:30 -0700

    Big Changes, New AwardList Class, new functions to tasklist

--- TaskList.cpp
+++ TaskList.cpp
@@ -1,3 +1,40 @@
 #include "TaskList.h"
 #include <iostream>
 using namespace std;
+
+TaskList::TaskList()
+{
+    head = nullptr;
+}
+
+TaskList::~TaskList()
+{
+    TaskNode* currNode = head;
+    TaskNode* prevNode = head;
+    while(currNode != nullptr)
+    {
+        prevNode = currNode;
+        currNode = currNode->next;
+        delete prevNode;
+    }
+}
+
+void TaskList::addTask(string name)
+{
+
+}
+
+void TaskList::deleteTask(string name)
+{
+
+}
+
+void TaskList::importTasks(ifstream& file)
+{
+
+}
+
+void TaskList::exportTasks(ofstream& file)
+{
+
+}
--- TaskList.h
+++ TaskList.h
@@ -2,9 +2,11 @@
 #define TASKLIST_H
 
 #include <iostream>
+#include<string>
 using namespace std;
 
 struct TaskNode {
+    enum tags
     string title;
     int month; //00
     int day; //00
@@ -15,18 +17,50 @@ struct TaskNode {
     int time;
     bool overdue;
     TaskNode* next;
-    void exportTask();
+    string exportTask()
+    {
+        string isDone;
+        string isOverdue;
+        if(done == true)
+        {
+            isDone = "true";
+        }
+        else
+        {
+            isDone = "false";
+        }
+        if(overdue == true)
+        {
+            isOverdue = "true";
+        }
+        else
+        {
+            isOverdue = "false";
+        }
+        string ouput = title + '`' + description + '`' + tag + '`' + isDone + '`' + to_string(month) + '`' + to_string(day) + '`' + to_string(year) + '`' + isOverdue + '`' + to_string(time);
+    };
+    int markCompleted()
+    {
+        
+    }
     TaskNode(string name, int d, int m, int y, string des, string tag, int dueTime) : title(name), month(m), day(d), year(y),
         description(des), tag(tag), time(dueTime), done(false), overdue(false), next(nullptr) {}
+
 };
+
+//enum tags{};
 
 class TaskList {
     private:
         TaskNode* head;
         int totalPoints;
     public:
+        TaskList();
+        ~TaskList();
         void addTask(string name);
         void deleteTask(string name);
+        void importTasks(ifstream& file);
+        void exportTasks(ofstream& file);
 
 };
 
