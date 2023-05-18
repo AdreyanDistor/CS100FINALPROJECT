acbcccd5a9d17a0dad77b7230bd6e7409d9d2209
ogod1 <ogodh001@ucr.edu>
2023-05-17 21:02:42 -0700

    markTaskCompleted method first version

--- TaskList.cpp
+++ TaskList.cpp
@@ -1,5 +1,6 @@
 #include "TaskList.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int TaskList::getTotalPoints() const {
@@ -8,12 +9,25 @@ int TaskList::getTotalPoints() const {
 
 void TaskList::markTaskCompleted(string name) {
     TaskNode* curr = head;
-    while (curr != nullptr || curr->title != name) {
+    string wholeTask;
+    ifstream inFS;
+    int count = 1;
+    inFS.open("Tasklist.txt");
+    if(!inFS.is_open()) {
+        cout << "error opening file" << endl;
+        return;
+    }
+    while (curr != nullptr || curr->title != name) { //finding the task in question 
         curr = curr->next;
+        count++;
     }
     if (curr == nullptr) {
         cout << "task does not exist try again." << endl; //FIX - give an error or something 
     }
-    curr->done = true;
+    curr->done = true; 
+    // for (int i = 0; i < count; i++) {
+    //     getline(inFS, wholeTask);
+    // }
     totalPoints += curr->value;
+    deleteTask(name);
 }
