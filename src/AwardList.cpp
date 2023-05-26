6a4e547bf3f3b2275657de04cb7c589a187103d3
slow-poker <nolanav1@gmail.com>
2023-05-26 16:11:45 -0700

    added buyAward()

--- src/AwardList.cpp
+++ src/AwardList.cpp
@@ -5,5 +5,17 @@ AwardList::AwardList()
     
 }
 
+AwardList::buyAward(string buyingAward, int totalPoints) {
+    unsigned i = 0;
+    while(awardlist.at(i).award_name != buyingAward && i < awardlist.size()) { //find specified award in the vector
+        ++i;
+    }
+
+    if(totalPoints >= awardlist.at(i).cost) { 
+        totalPoints -= awardlist.at(i).cost;
+        awardlist.at(i).user_count++;
+    }
+    return;
+}
 
 
