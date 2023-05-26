1906568a5f07e9ee6ef13a0685301340f61cfdc2
Adreyan Distor <adreyandistor@gmail.com>
2023-05-25 19:22:51 -0700

    TEST

---
+++ src/CMakeFiles/simpleLibrary.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/csmajs/adist003/final-project-scelo002-nvern003-adist003-ogodh001/src/AwardList.cpp" "src/CMakeFiles/simpleLibrary.dir/AwardList.cpp.o" "gcc" "src/CMakeFiles/simpleLibrary.dir/AwardList.cpp.o.d"
+  "/home/csmajs/adist003/final-project-scelo002-nvern003-adist003-ogodh001/src/TaskList.cpp" "src/CMakeFiles/simpleLibrary.dir/TaskList.cpp.o" "gcc" "src/CMakeFiles/simpleLibrary.dir/TaskList.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
