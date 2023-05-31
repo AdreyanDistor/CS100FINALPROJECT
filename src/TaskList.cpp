afbb4e4cc1ddd3c3ef9b9ff96951eba8abf263c1
AdreyanDistor <adreyandistor@gmail.com>
2023-05-31 11:06:38 -0700

    ADDED GUI CLASSES

--- src/TaskList.cpp
+++ src/TaskList.cpp
@@ -151,34 +151,6 @@ void TaskList::deleteTask(string name)
     }
 }
 
-void TaskList::sortByTag(string user_tag)
-{
-    TaskNode* currNode = head;
-    if(currNode != nullptr)
-    {
-        if(head == tail)
-        {
-            if(currNode->tag == user_tag)
-            {
-                cout << currNode->exportTask() << endl;
-            }
-        }
-        else
-        {
-            while(currNode != nullptr)
-            {
-                if(currNode->tag == user_tag)
-                {
-                    cout << currNode->exportTask() << endl;
-                }
-                currNode = currNode->next;
-            }
-        }
-    }
-    
-   
-    return;
-}
 
 
 // COULD ADD TO GUI CLASS
