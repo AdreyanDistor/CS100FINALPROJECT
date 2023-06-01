7afbcf6b12178f3be45a505806b6dff3a3653315
AdreyanDistor <adreyandistor@gmail.com>
2023-06-01 10:51:02 -0700

    START SCREEN

--- Raylib/src/main.cpp
+++ Raylib/src/main.cpp
@@ -1,70 +1,95 @@
 #include "../raylib/raylib/src/raylib.h"
-
+#include "../../header/AwardList.h"
+#include "../../header/AwardListGUI.h"
+#include "../../header/TaskList.h"
+#include "../../header/TaskListGUI.h"
 #include <vector>
 using namespace std;
 #include <string>
+#define MAX_INPUT_CHARS 20
+#include <cstring>
+
 int main()
 {
+    // Initialization
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "Infinite Scroll Example");
+    InitWindow(screenWidth, screenHeight, "Start Screen");
 
-    int scrollOffset = 0;         // Vertical scroll offset
-    int buttonHeight = 40;        // Height of each button
-    int buttonSpacing = 10;       // Spacing between buttons
-    int visibleButtons = screenHeight / (buttonHeight + buttonSpacing);  // Number of buttons visible on the screen at a time
+    char timeInput[MAX_INPUT_CHARS + 1] = "\0";
+    char dayInput[MAX_INPUT_CHARS + 1] = "\0";
+    bool goToNextScreen = false;
 
-    std::vector<std::string> buttonNames;  // Vector to store the names of the buttons
-    int totalButtons = visibleButtons;     // Total number of buttons to generate initially
+    Rectangle nextButtonRec = { screenWidth / 2 - 50, screenHeight - 100, 100, 50 };
 
-    // Generate initial buttons
-    for (int i = 0; i < totalButtons; i++)
-    {
-        buttonNames.push_back("Button " + std::to_string(i));
-    }
+    SetTargetFPS(60);
 
+    // Main game loop
     while (!WindowShouldClose())
     {
-        // Handle scrolling input with mouse wheel
-        scrollOffset += GetMouseWheelMove() * 3;
-
-        // Generate more buttons if scrolling down
-        if (scrollOffset > buttonNames.size() - visibleButtons)
+        // Update
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
         {
-            int numButtonsToGenerate = scrollOffset - (buttonNames.size() - visibleButtons);
+            Vector2 mousePos = GetMousePosition();
 
-            for (int i = 0; i < numButtonsToGenerate; i++)
+            if (CheckCollisionPointRec(mousePos, nextButtonRec))
             {
-                buttonNames.push_back("Button " + std::to_string(buttonNames.size()));
+                goToNextScreen = true;
             }
         }
 
-        // Clear the screen
+        // Draw
         BeginDrawing();
+
         ClearBackground(RAYWHITE);
 
-        // Render the visible buttons based on the scroll offset
-        int startIndex = scrollOffset;
-        int endIndex = startIndex + visibleButtons;
+        DrawText("Enter Time:", screenWidth / 2 - 80, screenHeight / 2 - 20, 20, BLACK);
+        DrawText(timeInput, screenWidth / 2 - 80, screenHeight / 2 + 10, 20, BLACK);
 
-        for (int i = startIndex; i < endIndex; i++)
+        DrawText("Enter Day:", screenWidth / 2 - 80, screenHeight / 2 + 60, 20, BLACK);
+        DrawText(dayInput, screenWidth / 2 - 80, screenHeight / 2 + 90, 20, BLACK);
+
+        DrawRectangleRec(nextButtonRec, LIGHTGRAY);
+        DrawText("Next", nextButtonRec.x + 20, nextButtonRec.y + 10, 20, BLACK);
+
+        EndDrawing();
+
+        // Input
+        if (!goToNextScreen)
         {
-            // Check if the button index is within the total number of buttons
-            if (i < buttonNames.size())
-            {
-                // Calculate the button position based on the index and scroll offset
-                int buttonY = (i - startIndex) * (buttonHeight + buttonSpacing);
+            int key = GetKeyPressed();
 
-                // Render the button
-                DrawRectangle(0, buttonY, screenWidth, buttonHeight, LIGHTGRAY);
-                DrawText(buttonNames[i].c_str(), 10, buttonY + 10, 20, DARKGRAY);
+            if (key != KEY_NULL && strlen(timeInput) < MAX_INPUT_CHARS)
+            {
+                timeInput[strlen(timeInput)] = (char)key;
+            }
+
+            if (IsKeyPressed(KEY_BACKSPACE))
+            {
+                int length = strlen(timeInput);
+                if (length > 0)
+                {
+                    timeInput[length - 1] = '\0';
+                }
+            }
+
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                strcpy(dayInput, timeInput);
+                memset(timeInput, '\0', sizeof(timeInput));
             }
         }
 
-        EndDrawing();
+        // Go to next screen
+        if (goToNextScreen)
+        {
+            // Add code to transition to the next screen
+            break;
+        }
     }
 
+    // De-Initialization
     CloseWindow();
 
     return 0;
