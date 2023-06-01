3b4cf0fb858db35b68ffab98964e704f6207e13e
isabellecelo <isabelle.celo04@gmail.com>
2023-06-01 16:29:29 -0700

    adding AddandDeleteAwards functions / the unit tests

--- header/AwardList.h
+++ header/AwardList.h
@@ -31,10 +31,15 @@ class AwardList
 		void importAwards(); //import tasks from “AwardsList.txt”
 		void exportAwards(); //will write the file within the function named “AwardsList.txt”
 		int buyAward(string name);//returns cost of ward, this is subtracted from total_points
-		void  useAward(string name); //idk what this would be, as of now it just prints a message I think, or the name, also remove it from the list
-		void createAward(string name, int cost); //add award to award list, if it’s already in, user_count++, cost can't be <  1
-		void deleteAward(string name); //delete award from list, if it’s already in the list, user_count–
+		void  useAward(string name); //decrement the count of a specific award
+		void createAward(string name, int cost); //add award to award list at the end
+		void deleteAward(string name); //delete award from list, shift the rest of the awards down the list
 		bool inList(string name); //return true if the award is in already in the list, false otherwise
+
+		// for testing
+		int getAwardListSize();
+		vector<Award*> getAwardList();
+
 };
 	
 	
--- test/test.cpp
+++ test/test.cpp
@@ -9,7 +9,43 @@
 #include<time.h>
 #include<ctime>
 using namespace std;
-int main() {
 
-    return 0;
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
+
+TEST(createAwardTest, testAwardSucessfullyCreated) {
+    TaskList t1;
+    t1.createAward("test award", 50);
+    EXPECT_EQ(awardlistSize, 1);
+    EXPECT_EQ(t1.getAwardList.at(0)->award_name, "test award");
+}
+
+TEST(createAwardTest, testAwardPushBack) {
+    TaskList t1;
+    t1.createAward("test award", 50);
+    t1.createAward("test award 2", 75);
+    EXPECT_EQ(awardlistSize, 2);
+    EXPECT_EQ(t1.getAwardList().at(1)->award_name, "test award 2" );
+}
+
+TEST(deleteAwardTest, testAwardDeleted) {
+    TaskList t1;
+    t1.createAward("test award", 50);
+    EXPECT_EQ(awardlistSize, 1);
+    t1.deleteAward("test award");
+    EXPECT_EQ(awardlistSize, 0);
+}
+
+TEST(deleteAwardTest, testCorrectAwardDeleted) {
+    TaskList t1;
+    t1.createAward("test award", 50);
+    t1.createAward("test award 2", 75);
+    t1.createAward("test award 3", 100);
+    t1.deleteAward("test award");
+    EXPECT_EQ(awardlistSize, 2);
+    EXPECT_EQ(t1.getAwardList.at(0)->award_name, "test award 2");
+}
+
+
