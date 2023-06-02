6ad063f0bc75b193fd4d3b1d88c0fadf81746526
ogod1 <ogodh001@ucr.edu>
2023-06-02 04:11:07 -0700

    Made a test

--- test/test.cpp
+++ test/test.cpp
@@ -15,7 +15,18 @@
 #include <time.h>       /* time */
 using namespace std;
 
-TEST(showOverdue, oneNode) {
-    TaskListGUI list; 
+TEST(showOverDue, test) {
+    TaskListGUI list;
+    list.addTask("one", "chore", "first one", 1, 1, 2022);
+    list.addTask("two", "chore", "second one", 3, 6, 2023);
+    list.addTask("three", "chore", "third one", 22, 11, 2023);
+    char* tm = "Fri Aug 15 00:00:00 2023";
+    list.showOverdue(tm);
+    TaskNode* curr1 = list.search("one");
+    TaskNode* curr2 = list.search("two");
+    TaskNode* curr3 = list.search("three");
+    EXPECT_EQ(curr1->overdue, true);
+    EXPECT_EQ(curr2->overdue, true);
+    EXPECT_EQ(curr3->overdue, false);
 }
 
