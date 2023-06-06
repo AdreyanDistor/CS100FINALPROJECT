f3228a33cf513cb5ac8c46ac71f99f4e5dcb2405
slow-poker <46554183+slow-poker@users.noreply.github.com>
2023-06-05 20:03:03 -0700

    Updated AwardList.h

--- header/AwardList.h
+++ header/AwardList.h
@@ -24,19 +24,22 @@ struct Award
 class AwardList
 {
 	private:
-		vector<Award> awardlist;
+		vector<Award*> awardVector;
+		int totalPoints;
 	public:
 		AwardList();
+		~AwardList();
 		void importAwards(ifstream& file); //import tasks from “AwardsList.txt”
 		void exportAwards(); //will write the file within the function named “AwardsList.txt”
-		int buyAward();//returns cost of ward, this is subtracted from total_points
-		void  useAward(); //idk what this would be, as of now it just prints a message I think, or the name, also remove it from the list
-		void createAward(); //add award to award list, if it’s already in, user_count++, cost can't be <  1
+		void buyAward(string buyingAward, int quantity); //increases user_count by 1, subtracts from totalPoints, error message if not enough points
+		void useAward(string name); //lower user_count by 1, erorr message if 0
+		void createAward(string name, int cost); //add award to award list
 		void deleteAward(string name); //delete award from list, if it’s already in the list, user_count–
 		bool inList(string name); //return true if the award is in already in the list, false otherwise
+		int getUserCount(int vecIndex) {return awardVector.at(vecIndex)->user_count;} // returns user_count of award at vecIndex (created for testing)
+		int getTotalPoints() {return totalPoints;}
+		void setTotalPoints(int newTotalPoints) {totalPoints = newTotalPoints;}
+		void importTotalPoints();
 };
 	
-	
-
-
 #endif
