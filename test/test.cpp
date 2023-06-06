9166b83bf533d70b40eb4fff1dec7b3bbae055be
slow-poker <46554183+slow-poker@users.noreply.github.com>
2023-06-05 20:02:23 -0700

    added buyAwardTest 1

--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,4 @@
+#include "googletest/googletest/include/gtest/gtest.h"
 #include "../header/AwardList.h"
 #include "../header/TaskList.h"
 #include<iostream>
@@ -10,6 +11,22 @@
 #include<ctime>
 using namespace std;
 int main() {
-
     return 0;
 }
+
+//TESTING: buyAward()
+TEST(buyAwardTest, testAwardSuccessfullyBought) {
+    AwardList testAwardList;
+    testAwardList.createAward("cupcake", 10);
+    testAwardList.setTotalPoints(10);
+
+    testAwardList.buyAward("cupcake"); //error in buyAward
+    EXPECT_EQ(testAwardList.getTotalPoints(), 0);
+    EXPECT_EQ(testAwardList.getUserCount(0), 1);
+}
+
+// TEST(buyAwardTest, testNotEnoughMoney) {
+//     AwardList testAwardList;
+
+// }
+
