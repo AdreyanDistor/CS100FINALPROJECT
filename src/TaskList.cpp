f7bf15758967df867692bd6afb1fffae685b9aff
Adreyan Distor <adreyandistor@gmail.com>
2023-06-09 16:09:27 -0700

    DECONSTRUCTOR CHANGES

--- src/TaskList.cpp
+++ src/TaskList.cpp
@@ -22,6 +22,10 @@ TaskList::~TaskList()
         delete currNode;
         currNode = next;
     }
+    if(recent_deleted_Task != nullptr)
+    {
+        delete recent_deleted_Task;
+    }
 }
 
 void TaskList::addTask(string name, string tag,string description, int day, int month, int year)
